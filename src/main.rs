ee82b69ed9aa365b3faafa62c19c191c9b379b02
Minghui Zhu <minghui.zhu@duke.edu>
2023-02-15 16:24:24 +0000

    ldoc project

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use chrono::{Datelike, Local, NaiveDate, Weekday};
+
+fn main() {
+    // LDOC is April 26, 2023
+    let ldoc = chrono::NaiveDate::from_ymd_opt(2023, 4, 26).unwrap();
+
+    // Spring break is from March 11 to March 19, 2023
+    let start_break = NaiveDate::from_ymd_opt(2023, 3, 11).unwrap();
+    let end_break = NaiveDate::from_ymd_opt(2023, 3, 19).unwrap();
+
+    // Get today's date
+    let today = Local::now().naive_local().date();
+  
+    // Calculate the number of days and weekdays until LDOC, excluding spring break from weekdays
+    let mut days_until_ldoc = 0;
+    let mut weekdays_until_ldoc = 0;
+    let mut date = today;
+    while date < ldoc {
+        days_until_ldoc += 1;
+        if date.weekday() != Weekday::Sat && date.weekday() != Weekday::Sun {
+            if date < start_break || date > end_break {
+                weekdays_until_ldoc += 1;
+            }
+        }
+        date = date + chrono::Duration::days(1);
+    }
+
+    println!(
+        "There are {} days ({} weekdays) until LDOC.\nHang in there Blue Devils!",
+        days_until_ldoc, weekdays_until_ldoc
+    );
+}
+
